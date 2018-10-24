e0a80db5337366a7546f98527ca6a60d74c1db5b
andacx <zhangwenchao@andacx.com>
2018-10-24 14:00:32 +0800

    alert

---
+++ CustomizeAlertView/CustomizeAlertView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomizeAlertView
+//
+//  Created by andacx on 2018/10/24.
+//  Copyright © 2018 andacx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CustomizeAlertView/CustomizeAlertView/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  CustomizeAlertView
+//
+//  Created by andacx on 2018/10/24.
+//  Copyright © 2018 andacx. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
